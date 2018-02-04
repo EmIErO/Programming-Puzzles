54b354df6796c1a57e97e6bdec6638ac7f1d3dc8
Mirosława Bryja <mira.bryja@gmail.com>
2018-02-04 21:45:23 +0100

    Add docstrings and comments

--- corruption_checksum_part_2.py
+++ corruption_checksum_part_2.py
@@ -1,29 +1,39 @@
+# Program converts txt file with data to a table (list of lists).
+# Then it finds the only two numbers in each row of the table where one evenly divides the other  
+# and adds up each row's result
+
 import corruption_checksum
 
-def calculate_quotient(row):
 
+def calculate_quotient(row):
+"""
+Finds the only two numbers in a row where one evenly divides the other.
+"""
     quotient = 0
 
     for i in range(len(row)):
             for j in range(len(row)):
-                if i != j and row[i] % row[j] == 0:
+                if i != j and row[i] % row[j] == 0: # doesn't allow dividing a number by itself, finds if different numbers divide evenly
                     quotient += row[i] / row[j]
                 
     return quotient
 
 
 def prepare_list_of_quotients(table):
+    """
+    Prepares list of quotients for every row in the given table.
+    """
 
     list_of_quotients = [calculate_quotient(row) for row in table]
-    print(list_of_quotients)
 
     return list_of_quotients
 
     
 def main():
+
     table = corruption_checksum.convert_data_to_table("corruption_checksum.txt")
     list_of_quotients = prepare_list_of_quotients(table)
-    checksum = sum(list_of_quotients)
+    checksum = sum(list_of_quotients) # calculates sum of all quotients
     print(checksum)
     
 
