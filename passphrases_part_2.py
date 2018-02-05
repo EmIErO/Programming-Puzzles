cfae30ff85b16d39d432b08e23f2017cd2df242e
Mirosława Bryja <mira.bryja@gmail.com>
2018-02-05 13:31:47 +0100

    Add count_valid_passphrases(), add condition to compare_if_anagrams(), change nomenclature and delete condition in find_anagrams()

--- passphrases_part_2.py
+++ passphrases_part_2.py
@@ -3,21 +3,34 @@ import passphrases
 
 def compare_if_anagrams(word1, word2):
     
-    n = len(word1)
-    same_letters = 0
-    
-    for letter in word1:
-        if letter in word2 and word1.count(letter) == word2.count(letter):
-            same_letters += 1
-    
-    if same_letters == n:
-        return True
+    if len(word1) == len(word2):
+        
+        n = len(word1)
+        same_letters = 0
+        
+        for letter in word1:
+            if letter in word2 and word1.count(letter) == word2.count(letter):
+                same_letters += 1
+        
+        if same_letters == n:
+            return True
 
 
-def find_anagrams(row):
+def find_anagrams(list_of_words):
     
-    for i in range(len(row) - 1):
-        for j in range(i+1, len(row)):
-            if len(row[i]) == len(row(j)) and compare_if_anagrams(row[i], row[j])
+    for i in range(len(list_of_words) - 1):
+        for j in range(i+1, len(list_of_words)):
+            if compare_if_anagrams(list_of_words[i], list_of_words[j])
                 return True
+
+def count_valid_passphrases(table):
+
+    all_passphrases = len(table)
+    valid_passphrases = all_passphrases
+
+    for row in table:
+        if find_anagrams(row):
+            valid_passphrases -= 1
+
+    return valid_passphrases
 
