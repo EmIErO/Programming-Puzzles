9e883805ef1d38644bbda66a44d62454a8bc1756
Mirosława Bryja <mira.bryja@gmail.com>
2018-02-05 14:51:11 +0100

    Add convert_data_to_list(), count_steps()

--- maze.py
+++ maze.py
@@ -1,18 +1,28 @@
-L = [0, 3, 0, 1, -3] 
+def convert_data_to_list(file_name):
 
-steps = 0
-old_position = 0
+    with open(file_name, "r") as file:
+        list_of_steps = []
+        for line in file:
+            step = file.readline()
+            step = int(step.replace("\n", ""))
+            list_of_steps.append(step)
+        
+    return list_of_steps
 
 
-while old_position <= len(L):
-    try:
-        new_position = old_position + L[old_position]
-        L[old_position] = L[old_position] + 1
+def count_steps(list_):
+        
+    steps = 0
+    old_position = 0
+
+    while old_position < len(list_):
+    
+        new_position = old_position + list_[old_position]
+        list_[old_position] = list_[old_position] + 1
         old_position = new_position
         
         steps +=1
-        
-    except IndexError:
-        break
 
-print("Getting out took {} steps.".format(steps)) 
+    print("Getting out took {} steps.".format(steps)) 
+
+convert_data_to_list("maze.txt")
