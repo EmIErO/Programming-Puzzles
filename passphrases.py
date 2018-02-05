86fe0059ede61f7358d0ba444f263446183f764a
Mirosława Bryja <mira.bryja@gmail.com>
2018-02-05 12:56:16 +0100

    Add compare_phrases(), count_valid_passphrases() and main()

--- passphrases.py
+++ passphrases.py
@@ -4,8 +4,35 @@ def convert_data_to_table(file_name):
         lines = file.readlines()
 
     data_table = [line.replace("\n", "").split(" ") for line in lines]
-    print(data_table)
 
     return data_table
 
-convert_data_to_table("passphrases.txt")
+
+def compare_phrases(row):
+    for i in range(len(row) - 1):
+        for j in range(i+1, len(row)):
+            if row[i] == row[j]:
+                return True
+        
+
+
+def count_valid_passphrases(table):
+
+    all_passphrases = len(table)
+    valid_passphrases = all_passphrases
+    for row in table:
+        if compare_phrases(row):
+            valid_passphrases -= 1
+
+    return valid_passphrases
+
+
+def main():
+
+    table = convert_data_to_table("passphrases.txt")
+    valid_passphrases = count_valid_passphrases(table)
+    print(valid_passphrases)
+
+
+if __name__ == '__main__':
+    main()
