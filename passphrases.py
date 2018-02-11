e4174883b65be459997c6658852551d21298961c
Mirosława Bryja <mira.bryja@gmail.com>
2018-02-11 19:26:36 +0100

    Add docstrings and comments

--- passphrases.py
+++ passphrases.py
@@ -1,5 +1,12 @@
-def convert_data_to_table(file_name):
+# Program checks if passphrase that consists of a series of words (lowercase letters) 
+# separated by spaces contains no duplicate words.
+# Phassphrases to check are given as a txt file.
 
+def convert_data_to_table(file_name):
+    """
+    Converts data from txt file to list of lists.
+    Returns list of lists with data.
+    """
     with open(file_name, "r") as file:
         lines = file.readlines()
 
@@ -9,6 +16,9 @@ def convert_data_to_table(file_name):
 
 
 def compare_phrases(row):
+    """
+    Compares phrases from a list (row), if duplicate phrases are found, returns True.
+    """
     for i in range(len(row) - 1):
         for j in range(i+1, len(row)):
             if row[i] == row[j]:
@@ -17,6 +27,10 @@ def compare_phrases(row):
 
 
 def count_valid_passphrases(table):
+    """
+    Counts passphrases without duplicates.
+    Returns number of valid passphrases.
+    """
 
     all_passphrases = len(table)
     valid_passphrases = all_passphrases
